d79c7f548cd65e5b2c1a06b84b94d52f570abb37
Jaan Altosaar <jaan@onefact.org>
2023-07-24 11:58:55 -0400

    update with example of replacing nullstr values with null string

---
+++ datathinking/models/neighborhoodatlas.medicine.wisc.edu/area_deprivation_index.sql
@@ -0,0 +1,28 @@
+-- downloaded from https://www.neighborhoodatlas.medicine.wisc.edu/download
+{{ config(materialized='external', location='/tmp/area_deprivation_index.parquet') }}
+SELECT 
+  INDEX,
+  GISJOIN,
+  regexp_replace(ADI_NATRANK, '^(GQ|PH|QDI|GQ-PH)$', 'NULL') as ADI_NATRANK, 
+  regexp_replace(ADI_STATERNK, '^(GQ|PH|QDI|GQ-PH)$', 'NULL') as ADI_STATERNK,
+  FIPS,
+FROM read_csv('/Users/me/Downloads/adi-download/US_2021_ADI_Census Block Group_v4.csv',
+  header=True,
+  delim=',',
+  quote='"',
+  columns={'INDEX': 'INT',
+           'GISJOIN': 'VARCHAR',
+           'ADI_NATRANK': 'VARCHAR',
+           'ADI_STATERNK': 'VARCHAR',
+           'FIPS': 'VARCHAR'}
+)
+-- FROM read_csv('/Users/me/Downloads/adi-download/US_2021_ADI_Census Block Group_v4.csv', 
+--   header=True, 
+--   delim=',',
+--   quote='"',
+--   columns={'INDEX': 'INT',
+--            'GISJOIN': 'VARCHAR',
+--            'ADI_NATRANK': 'VARCHAR',  
+--            'ADI_STATERNK': 'VARCHAR',
+--            'FIPS': 'VARCHAR'})
+-- -- LIMIT 10;
